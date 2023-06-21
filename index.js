881fb54444f7d27fe0f026f7f228e472ed75591c
GonzaloBounzerio <bounzeriog@gmail.com>
2023-06-21 10:07:43 -0300

    2daPreentrega

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 
-function vendePrenda(){
-    let venta=prompt(`Cuántas remeras quieren comprar?`)
-    console.log(`Se vendieron ${venta} remeras`)
-    if (venta>=5) {
-        console.log(`Cliente de lujo`)
+class prenda { 
+    constructor(id, tipo, talle, precio){
+        this.id = id;
+        this.tipo = tipo;
+        this.talle = talle;
+        this.precio = precio;
+    }
+}
+
+const prendas = [];
+
+let prendasDiv = document.getElementById("stock")
+
+function ingresoPagina(prendas){
+    for (let prenda of prendas){
+        let nuevoDiv = document.createElement("div")
+        nuevoDiv.className = "content"
+        nuevoDiv.innerHTML = `<div class="prendaStock" id="${prenda.id}">
+                                <div class="data">
+                                    <h5>${prenda.tipo}</h5>
+                                    <h5>${prenda.talle}</h5>
+                                    <h6>${prenda.precio}</h5>
+                                </div>
+                              </div>`
+    prendasDiv.appendChild(nuevoDiv)    
     }
 }
 
 function ingresaPrenda(){
-    let ingreso=prompt(`Cuántas remeras entraron?`)
-    console.log(`Ingresaron ${ingreso} remeras`)
-    if (ingreso<5) {
-        console.log(`Ingresaron pocas remeras :(`)
-    }
+    let ingreso=prompt(`¿Qué prenda ingresó?`)
+    let talle = prompt(`¿Qué talle?`)
+    let precio = parseInt(prompt(`Indique precio`))
+    const nuevaPrenda = new prenda(prendas.length+1,ingreso.toUpperCase(),talle.toUpperCase(),precio)
+    prendas.push(nuevaPrenda)
 }
 
 let salirMenu = false
@@ -20,14 +40,10 @@ alert("Bienvenido")
 
 do{
     let opcion = prompt(`Ingrese la opción deseada
-   1 - Vender remeras
-   2 - Ingresar remeras
+   1 - Ingresar prendas
    0 - Salir`)
    switch (opcion) {
         case "1":
-            vendePrenda();
-            break;
-        case "2":
             ingresaPrenda();
             break;
         case "0":
@@ -39,3 +55,4 @@ do{
             break;
    }
 }while(!salirMenu)
+ingresoPagina(prendas)
