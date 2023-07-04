4993c1edb4f000113a4a10535250669cc7dfdf02
GonzaloBounzerio <bounzeriog@gmail.com>
2023-07-03 21:59:24 -0300

    3raPreentrega

--- index.js
+++ index.js
@@ -44,35 +44,71 @@ function imprime(array){
                                     <h4>${movies.year}</h4>
                                 </div>
                                 <div class="btnMiniCard">
-                                    <span class="agregaFav material-symbols-outlined">add</span>
+                                    <label id="agregaFav${movies.id}" class="material-symbols-outlined">add</label>
                                 </div>
                             </div>`
     moviesMinDiv.appendChild(nuevoDiv)
+
+    let btnAgregaFav = document.getElementById(`agregaFav${movies.id}`)
+    btnAgregaFav.addEventListener("click", () => {
+        document.getElementById(`agregaFav${movies.id}`).innerHTML=``
+        agregaFavArray(movies)
+    })
+
     }
 }
 
+let arrayFav = []
+let divFav = document.getElementById("containerFav")
 
+function agregaFavArray(movieAdd){
 
-    for (let data of arrayAllMovies){
-        let divFav = document.getElementById("containerFav")
-        let nuevoDivFav = document.createElement("div")
-        nuevoDivFav.className="movieCard"
-        nuevoDivFav.innerHTML=`<div class="dataMovie" id="${data.id}">
-                                 <div class="imagenMovie">
-                                     <img src="${data.image}" alt="logo ${data.title}" width="80%">
-                                 </div>
-                                 <div class="containerData">
-                                     <div class="barraData">
-                                         <div class="movieTitle" >
-                                             <h4>${data.title}</h4>
-                                             <h5>${data.director}</h5>
-                                             <h5>${data.year}</h5>
-                                         </div>
-                                         <div class="botonMovie" >
-                                             <label class="borrarMovie material-symbols-outlined">close</label>
-                                         </div>
-                                     </div>
-                                 </div>
-                             </div>`
-        divFav.appendChild(nuevoDivFav)
+    let movieAgregada = arrayFav.find((elem) => elem.id == movieAdd.id)
+    if (movieAgregada == undefined){
+        arrayFav.push(movieAdd)
+        imprimeFav(arrayFav)
+    }else{
+        document.getElementById(`agregaFav${movieAdd.id}`).innerHTML=``
+        alert("Ya fue agregada") // hola profe, cuando corrijas esta parte podrias indicarme como hacer para que no se vuelva a imprimir el logo de sumar, para no tener que usar un alert gracias
     }
+}        
+    
+    function imprimeFav(arrayFav){
+        divFav.innerHTML=``
+        for (let peli of arrayFav){
+            let nuevoDivFav = document.createElement("div")
+            nuevoDivFav.className="movieCard"
+            nuevoDivFav.innerHTML=`<div class="dataMovie" id="${peli.id}">
+                                    <div class="imagenMovie">
+                                        <img src="${peli.image}" alt="logo ${peli.title}" width="80%">
+                                    </div>
+                                    <div class="containerData">
+                                        <div class="barraData">
+                                            <div class="movieTitle" >
+                                                <h4>${peli.title}</h4>
+                                                <h5>${peli.director}</h5>
+                                                <h5>${peli.year}</h5>
+                                            </div>
+                                            <div class="botonMovie" >
+                                                <label id="borrarMovie${peli.id}" class="botonBorraFav material-symbols-outlined">close</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>`
+            divFav.appendChild(nuevoDivFav)
+        
+            let btnBorraFav = document.getElementById(`borrarMovie${peli.id}`)
+            btnBorraFav.onclick = () => {
+                borraArrayFav(arrayFav,peli.id)
+            }
+        }  
+    }
+
+
+//borraFav
+
+function borraArrayFav(arrayFav,num){
+    arrayFav = arrayFav.filter(pelicula => pelicula.id != `${num}`);
+    console.log(arrayFav)
+    imprimeFav(arrayFav)            //cuando borro no puedo volver a agregarla, idce que ya existe el id, por que es asi?
+}
